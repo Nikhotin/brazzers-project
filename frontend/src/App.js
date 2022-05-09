3da942aef39fab52cf90d6f8cfc15b45815c3b07
ruslan <bfsamara.srr@yandex.ru>
2022-05-09 17:57:32 +0300

    add react-router and fix app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import MainLayout from "./views/layouts/main/MainLayout";
-import { Route, Switch, withRouter } from 'react-router-dom';
-import signIn from 'views/pages/signIn/signIn';
+import {Route, Switch, withRouter} from "react-router";
+import signIn from './views/pages/signIn/signIn';
 
 function App() {
   return (
